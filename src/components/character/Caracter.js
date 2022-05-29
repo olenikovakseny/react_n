40ecb299ecb097c83c877df571680d5df2b6c9ea
ElenaYakub <lightcharcoal7@gmail.com>
2022-05-29 11:52:08 +0300

    heroes er

---
+++ src/components/character/Caracter.js
@@ -0,0 +1,12 @@
+export default function Character({ image, cName }) {
+    return (
+        <div>
+            <h2>{cName}</h2>
+            <img src={image} alt="" />
+            <p>lorem poikujytsdfghjkl;'
+                ';lkjhghjkl;'
+                ';l'
+            </p>
+        </div>
+    );
+}
---
+++ src/components/heroe/Heroe.js
@@ -0,0 +1,11 @@
+export default function Heroe({ item: char_id, name, img, status, nickname }) {
+    return (
+        <div>
+            <h2>{char_id}.  {name}</h2>
+            <img src={img} alt="" />
+            <p>{status}, {nickname}
+            </p>
+        </div>
+    );
+}
+
